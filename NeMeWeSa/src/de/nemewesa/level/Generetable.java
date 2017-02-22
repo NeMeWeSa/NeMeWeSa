d77af707cd735e107a834514d991e4a35a633ba5
NeMeWeSa <nemewesa@gmail.com>
2017-02-22 22:11:03 +0100

    Update Generetable.java

--- NeMeWeSa/src/de/nemewesa/level/Generetable.java
+++ NeMeWeSa/src/de/nemewesa/level/Generetable.java
@@ -3,6 +3,8 @@ package de.nemewesa.level;
 public interface Generetable {
 
 	//public abstract void generate();
+
+	public abstract String getName();
 	
 	public abstract void generate(int element);
 	
