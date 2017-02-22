821cf26bc14b224e838a4692b1346c129b558296
NeMeWeSa <nemewesa@gmail.com>
2017-02-22 22:12:58 +0100

    Update Ressource.java

--- NeMeWeSa/src/de/nemewesa/level/Ressource.java
+++ NeMeWeSa/src/de/nemewesa/level/Ressource.java
@@ -6,8 +6,8 @@ public class Ressource {
 	
 	public String resourceName;
 	/*
-	 * value	 = 	0 bronze, 1 = silber, 2= gold ..... je höher die zahl desto 
-	 * 				höher der wert
+	 * value	 = 	0 bronze, 1 = silber, 2= gold ..... je hoeher die zahl desto 
+	 * 				hoeher der wert
 	 * weigth 	= 	wieviel "platz" nimmt die ressource weg, oder wieviel kg/t 
 	 * 				wiegt eine einheit davon
 	 * grow		=	in welchen abstand kommt wieder eine einheit	
@@ -24,9 +24,9 @@ public class Ressource {
 		this.value = value;
 		this.space = space;
 		this.amount = amount;
-		// grow wird bestimmt durch den modolu später
-		// grow%4 = 0 kupfer wächst alle 4 Runden nach
-		// grow%6 = 0 silber wächst alle 4 Runden nach 
+		// grow wird bestimmt durch den modolu spaeter
+		// grow%4 = 0 kupfer waechst alle 4 Runden nach
+		// grow%6 = 0 silber waechst alle 4 Runden nach 
 		// .....
 		this.grow = grow;
 		this.dropRate = dropChance;
