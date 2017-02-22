f8d56015213a58f78e395c871b21d49f0e976e50
benjaminr <benjaminr@c-503-r2g2.cbm.local>
2017-02-22 13:56:00 +0100

    - Helper importiert - StringArray mit Namen hinzugefügt - namen generator erstellt - funktion die unter 6 zeichen noch eine weitere silbe anhängt

--- NeMeWeSa/src/de/nemewesa/level/Planet.java
+++ NeMeWeSa/src/de/nemewesa/level/Planet.java
@@ -2,6 +2,8 @@ package de.nemewesa.level;
 
 import java.util.ArrayList;
 
+import de.nemewesa.helper.Helper;
+
 public class Planet implements Generetable{
 	
 	public Moon moonName;
@@ -12,9 +14,10 @@ public class Planet implements Generetable{
 	public boolean minePosible;
 	public String name;
 	public Solarsystem parent;
+	public String[] firstname = {"Ben", "Pe", "To", "Jau", "Ja", "Ter", "Masz", "Res", "Min", "Ro", "Sen", "Ta", "Bue", "Ur", "Ban", "Iak", "Dzi", "Ko", "Wi", "Cki"};
 	
 	public Planet(String name, Solarsystem parent) {
-		this.name = name;
+		this.name = generateName();
 		this.parent = parent;
 	}
 
@@ -31,6 +34,18 @@ public class Planet implements Generetable{
 	@Override
 	public void printChildren() {
 		
+	}
+	public String generateName() {
+		String name1 = firstname[Helper.random(0, firstname.length)];
+		String name2 = firstname[Helper.random(0, firstname.length)];
+				if(name1.equals(name2)) {
+			name1 = firstname[Helper.random(0, firstname.length)];
+		}
+		String name4 = name1 + name2.toLowerCase();
+		if(name4.length()<7) {
+			return name4;
+		}
+		return name4+firstname[Helper.random(0, firstname.length)].toLowerCase();
 	}
 	
 
