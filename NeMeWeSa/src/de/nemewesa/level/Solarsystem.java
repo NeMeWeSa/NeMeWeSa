a6b5afeaf1b2cbec399237f30c496ec7ae2b8332
benjaminr <benjaminr@c-503-r2g2.cbm.local>
2017-02-22 11:00:18 +0100

    ...

--- NeMeWeSa/src/de/nemewesa/level/Solarsystem.java
+++ NeMeWeSa/src/de/nemewesa/level/Solarsystem.java
@@ -2,17 +2,20 @@ package de.nemewesa.level;
 
 import java.util.ArrayList;
 
+import de.nemewesa.helper.Helper;
+
 public class Solarsystem implements Generetable{
 	
 	public String name;
 	public Sector parent;
-//	private ArrayList<Planet> planets = new ArrayList<>();	
+//	private ArrayList<Planet> planets = new ArrayList<>();
+	public int plus3 = Helper.random(1, 3);
 	
 	public Solarsystem(String name, Sector parent){
 		this.name = name;
 		this.parent = parent;
 	}
-
+	
 	@Override
 	public void generate(int element) {
 		
