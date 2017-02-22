12c34452e7a4fcb7d7622b5933ff10e711284821
NeMeWeSa <nemewesa@gmail.com>
2017-02-22 22:10:32 +0100

    Update Player.java

--- NeMeWeSa/src/de/nemewesa/character/Player.java
+++ NeMeWeSa/src/de/nemewesa/character/Player.java
@@ -1,8 +1,11 @@
 package de.nemewesa.character;
 
 import de.nemewesa.app.App;
+import de.nemewesa.level.Generetable;
 import de.nemewesa.level.Planet;
 import de.nemewesa.level.Solarsystem;
+
+import java.util.ArrayList;
 
 public class Player {
 	
@@ -10,27 +13,42 @@ public class Player {
 	private Planet currentPlanet = null;
 	private Planet homePlanet = null;
 	private Solarsystem homeSolarsystem = null;
+	private ArrayList<Generetable> ownership = new ArrayList<>();
 	
 	private final transient int ap = App.PLAYER_AP;
 	
 	public Player(String name){
-		
 		this.name = name;
-		
 	}
 	
 	public String toString(){
 		String info = "";
 		info += "[NeMeWeSa] Du befindest dich auf den Planeten " + this.currentPlanet.name + ".\n";
 		info += "[NeMeWeSa] " + this.homePlanet.name + " ist dein Heimatplanet.\n";
-		info += "[NeMeWeSa] Dein Heimat - Solarsystem ist der " + this.homeSolarsystem.name + ".\n";
+		info += "[NeMeWeSa] Dein Heimat-Solarsystem ist der " + this.homeSolarsystem.name + ".\n";
+		info += "[NeMeWeSa] Dein Besitz > \n";
+		for(Generetable ownership : ownership){
+			info += ownership.getClass().getSimpleName() + " : " + ownership.getName() +"\n";
+		}
 		return info;
 	}
-	
+
+	public Generetable getOwnership(int index) {
+		return this.ownership.get(index);
+	}
+
+	public void addOwnership(Generetable ownership) {
+		this.ownership.add(ownership);
+	}
+
 	public void setCurrentPlanet(Planet currPlanet){
 		this.currentPlanet = currPlanet;
 	}
-	
+
+	public Planet getHomePlanet(){
+		return this.homePlanet;
+	}
+
 	public void setHomePlanet(Planet homePlanet){
 		this.homePlanet = homePlanet;
 	}
@@ -50,7 +68,5 @@ public class Player {
 	public int getAp() {
 		return ap;
 	}
-	
-	
 
 }
