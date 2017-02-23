9a2a66452f06dcade7845697fbfb20bedf780c9c
peterd <peterd@c-503-r1g1.cbm.local>
2017-02-23 11:58:30 +0100

    Nur die Starage Klasse die eine arrayList erzeugt von Resourcen. Nichts besonderes

---
+++ NeMeWeSa/src/de/nemewesa/level/Storage.java
@@ -0,0 +1,12 @@
+package de.nemewesa.level;
+
+import java.util.ArrayList;
+
+////Hier kann ich in die arrayList die rohstoffe plus die Anzahl speichern.
+public class Storage {
+
+	public ArrayList<Resource> resources = new ArrayList<>();
+	
+	
+	
+}
