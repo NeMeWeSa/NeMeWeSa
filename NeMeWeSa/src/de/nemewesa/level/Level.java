3fc8eba40ac16eeef31cf65a62ffae84dc122fb3
Tomasz <tomasz.urbaniak@me.com>
2017-02-24 16:51:12 +0100

    Pfade auf Mac gesetzt !!!

--- NeMeWeSa/src/de/nemewesa/level/Level.java
+++ NeMeWeSa/src/de/nemewesa/level/Level.java
@@ -27,10 +27,10 @@ public class Level implements Serializable{
 		String sectorname = null;
 
 		// Pfad unter Mac
-		// String filename = "level/" + Level.level + "/sectors/sectors.txt";
+		String filename = "level/" + Level.level + "/sectors/sectors.txt";
 
 		// Pfad unter Windows
-		String filename = "level\\" + Level.level + "\\sectors\\sectors.txt";
+		//String filename = "level\\" + Level.level + "\\sectors\\sectors.txt";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
 
